4d74c61c8ef83804fd24b71f1d5cf5d308d6eaa5
Andrew Forward <aforward@gmail.com>
2017-04-06 09:22:17 -0400

    Add a force to force a value to be updated

--- lib/changeset_merger.ex
+++ lib/changeset_merger.ex
@@ -38,4 +38,40 @@ defmodule ChangesetMerger do
       :else -> Map.put(params, field, default_if_missing)
     end
   end
+
+  @doc """
+  Force a field to be a certain value.
+
+  It will treat string and atoms as the same underlying value
+
+  ## Examples
+
+      iex> ChangesetMerger.force(%{}, "apples", "blue")
+      %{"apples" => "blue"}
+
+      iex> ChangesetMerger.force(%{}, :apples, "blue")
+      %{apples: "blue"}
+
+      iex> ChangesetMerger.force(%{"apples" => "green"}, "apples", "blue")
+      %{"apples" => "blue"}
+
+      iex> ChangesetMerger.force(%{apples: "green"}, "apples", "blue")
+      %{apples: "blue"}
+
+  """
+  def force(params, field, val) when is_atom(field) do
+    force(params, field, "#{field}", field, val)
+  end
+  def force(params, field, val) when is_binary(field) do
+    force(params, field, field, String.to_atom(field), val)
+  end
+  defp force(params, field, field_str, field_atom, val) do
+    cond do
+      Map.has_key?(params, field_str) -> Map.put(params, field_str, val)
+      Map.has_key?(params, field_atom) -> Map.put(params, field_atom, val)
+      :else -> Map.put(params, field, val)
+    end
+  end
+
+
 end
