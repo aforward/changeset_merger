f0959b6a0382501bd285d83eb004fa65e104ea68
Andrew Forward <aforward@gmail.com>
2017-04-06 09:01:45 -0400

    Initial commit

---
+++ lib/changeset_merger.ex
@@ -0,0 +1,18 @@
+defmodule ChangesetMerger do
+  @moduledoc """
+  Documentation for ChangesetMerger.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChangesetMerger.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/changeset_merger_test.exs
@@ -0,0 +1,8 @@
+defmodule ChangesetMergerTest do
+  use ExUnit.Case
+  doctest ChangesetMerger
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
