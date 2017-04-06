59723e339b5df968c21a0401593be4bb7a1090f0
Andrew Forward <aforward@gmail.com>
2017-04-06 16:16:57 -0400

    Adding to hex

--- mix.exs
+++ mix.exs
@@ -1,10 +1,19 @@
 defmodule ChangesetMerger.Mixfile do
   use Mix.Project
 
+  @git_url "https://github.com/aforward/changeset_merger"
+  @home_url @git_url
+  @version "0.1.0"
+
   def project do
     [app: :changeset_merger,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.4",
+     name: "ChangesetMerger",
+     description: "A library for common Ecto changeset transformations.",
+     package: package(),
+     source_url: @git_url,
+     homepage_url: @home_url,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -15,6 +24,15 @@ defmodule ChangesetMerger.Mixfile do
   end
 
   defp deps do
-    [{:mix_test_watch, "~> 0.3", only: :dev, runtime: false},]
+    [{:mix_test_watch, "~> 0.3", only: :dev, runtime: false},
+     {:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp package do
+    [name: :changeset_merger,
+     files: ["lib", "mix.exs", "README*", "README*", "LICENSE*"],
+     maintainers: ["Andrew Forward"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => @git_url}]
   end
 end
