28af4856736a4bb968018516724beae1eca0d837
Andrew Forward <aforward@gmail.com>
2018-07-07 08:37:52 -0400

    v0.3.15

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ChangesetMerger.Mixfile do
 
   @git_url "https://github.com/aforward/changeset_merger"
   @home_url @git_url
-  @version "0.3.14"
+  @version "0.3.15"
 
   def project do
     [
